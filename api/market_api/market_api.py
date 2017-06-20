592abbd8c688a6bde9f3fd5eb8b723096feadcbe
kokopelli314 <nacclonts@gmail.com>
2017-06-19 23:48:24 -0600

    Set up database initialization from CSV file.

--- api/market_api/market_api.py
+++ api/market_api/market_api.py
@@ -15,13 +15,55 @@ app.config.update(dict(
 ))
 app.config.from_envvar('MARKETS_API_SETTINGS', silent=True)
 
+
+
 def connect_db():
+    """Opens a new database connection."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+
+def init_db():
+    """Initialize database from CSV data file."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+
+    data_source = os.path.join(app.root_path, 'market_data.csv')
+    with open (data_source, 'r') as f:
+        reader = csv.reader(f)
+        cursor = db.cursor()
+        columns = next(reader)
+        # Add each column (except FMID, which is already assigned as primary key)
+        for column in columns:
+            if column != 'FMID':
+                add_query = 'alter table Markets add column %s varchar(255)' % column
+                cursor.execute(add_query)
+
+        # Insert data
+        query = 'insert into Markets({0}) values ({1})'
+        query = query.format(','.join(columns), ','.join('?' * len(columns)))
+        for data in reader:
+            cursor.execute(query, data)
+    # Save to database
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
 
 def get_db():
     """Opens a new database connection if there currently isn't one for the application context."""
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
--- api/market_api/schema.sql
+++ api/market_api/schema.sql
@@ -1,6 +1,4 @@
-drop table if exists entries;
-create table entries (
-    id integer primary key autoincrement,
-    title text not null,
-    'text' text not null
+drop table if exists Markets;
+create table Markets (
+    FMID integer primary key
 );
